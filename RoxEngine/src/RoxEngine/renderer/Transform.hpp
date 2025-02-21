73860b92e64a3b8c4959c07f2a907f4cb66cc8d5
Rodrigo Vieira <rodizzvieira@gmail.com>
2025-02-21 14:48:31 +0000

    Transform

---
+++ RoxEngine/src/RoxEngine/renderer/Transform.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+
+namespace RoxEngine {
+    class Transfom {
+    public:
+        Transfom() = default;
+        inline Transfom(glm::vec3 position, glm::vec3 rotation = {0,0,0}, glm::vec3 scale = {1,1,1}) {
+            this->position = position;
+            this->rotation = rotation;
+            this->scale = scale;
+        }
+        inline glm::mat4 RecalculateMatrix() {
+            return glm::translate(glm::mat4(1.0f), this->position) * glm::scale(glm::mat4(1.0f), scale) * glm::toMat4(glm::quat(this->rotation));
+        }
+
+        glm::vec3 position = {0,0,0};
+        glm::vec3 rotation = {0,0,0};
+        glm::vec3 scale = {1,1,1};
+    };
+}
