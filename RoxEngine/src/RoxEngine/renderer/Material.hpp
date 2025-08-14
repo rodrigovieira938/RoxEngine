e127a4eaf1e7eb24919f139a82c70ff539177c4d
rodrigovieira938 <rodizzvieira@gmail.com>
2025-08-14 17:17:19 +0100

    Fix Typo

--- RoxEngine/src/RoxEngine/renderer/Material.hpp
+++ RoxEngine/src/RoxEngine/renderer/Material.hpp
@@ -8,9 +8,9 @@
 namespace RoxEngine {
     class Material {
     public:
-        Material(alina::Shader vertex_shader, alina::Shader fragment_shader, Ref<ModuleReflection> moduleReflection) {mVertexShader = vertex_shader; mFragmentShader = vertex_shader; mModuleReflection = moduleReflection;}
+        Material(alina::Shader vertex_shader, alina::Shader fragment_shader, Ref<ModuleReflection> moduleReflection) {mVertexShader = vertex_shader; mFragmentShader = fragment_shader; mModuleReflection = moduleReflection;}
         inline alina::Shader GetVertexShader() {return mVertexShader;}    
-        inline alina::Shader GetFragmentShader() {return mVertexShader;}    
+        inline alina::Shader GetFragmentShader() {return mFragmentShader;}    
     private:
         alina::Shader mVertexShader, mFragmentShader;
         Ref<ModuleReflection> mModuleReflection;
