440c6ef0e57a532416e2abe82be94cdd3b35355e
Rodrigo Vieira <rodizzvieira@gmail.com>
2025-12-29 17:50:48 +0000

    feat: add multiple uv channels

--- RoxEngine/src/RoxEngine/renderer/Mesh.hpp
+++ RoxEngine/src/RoxEngine/renderer/Mesh.hpp
@@ -1,16 +1,54 @@
 #pragma once
 #include "alina/alina.hpp"
 #include <RoxEngine/utils/Utils.hpp>
+#include <array>
 #include <glm/glm.hpp>
 #include <vector>
 #include <RoxEngine/renderer/alina/ResourcePool.hpp>
+#include <RoxEngine/renderer/ShaderReflection.hpp>
 
 namespace RoxEngine {
     class Mesh {
     public:
         struct Data {
+        private:
+            template<typename T, std::size_t size, std::size_t index>
+            struct FieldAliasWorkaround {
+            public:
+                T* operator ->() {
+                    return &data[index];
+                }
+                operator T&() {
+                    return data[index];
+                }
+                operator const T&() const {
+                    return data[index];
+                }
+                FieldAliasWorkaround& operator=(const T& value) {
+                    data[index] = value;
+                    return *this;
+                }
+
+                FieldAliasWorkaround& operator=(T&& value) {
+                    data[index] = std::move(value);
+                    return *this;
+                }
+            private:
+                std::array<T, size> data;  
+            };
+        public:
+            Data() {
+                new(&uv) FieldAliasWorkaround<std::vector<glm::vec2>, ShaderReflection::VertexBindingPoint::UV_MAX, 0>();
+            }
+            ~Data() {
+                uv.~FieldAliasWorkaround();
+            }
             std::vector<glm::vec3> position;
-            std::vector<glm::vec2> uvs;
+            union {
+                std::array<std::vector<glm::vec2>, ShaderReflection::VertexBindingPoint::UV_MAX> uvs;
+                FieldAliasWorkaround<std::vector<glm::vec2>, ShaderReflection::VertexBindingPoint::UV_MAX, 0> uv;
+                FieldAliasWorkaround<std::vector<glm::vec2>, ShaderReflection::VertexBindingPoint::UV_MAX, 1> uv1;
+            };
             std::vector<glm::vec3> normals;
             std::vector<uint32_t> indices;
 
@@ -24,9 +62,9 @@ namespace RoxEngine {
                 cmd->begin();
                 cmd->writeBuffer(position_vb, position.data(), sizeof(glm::vec3) * position.size(), 0);
                 cmd->writeBuffer(indices_vb, indices.data(), sizeof(uint32_t) * indices.size(), 0);
-                if(uvs.size() == vertexCount) {
+                if(uv->size() == vertexCount) {
                     uvs_vb = device->createBuffer(alina::BufferDesc().setDebugName("MESH - UV VB").setType(alina::BufferType::VERTEX));
-                    cmd->writeBuffer(uvs_vb, uvs.data(), sizeof(glm::vec2) * uvs.size(), 0);
+                    cmd->writeBuffer(uvs_vb, uv->data(), sizeof(glm::vec2) * uv->size(), 0);
                 }
                 if(normals.size() == vertexCount) {
                     normals_vb = device->createBuffer(alina::BufferDesc().setDebugName("MESH - Normal VB").setType(alina::BufferType::VERTEX));
@@ -39,11 +77,11 @@ namespace RoxEngine {
         Mesh() : mData(CreateRef<Data>()) {}
         inline Mesh clone() {return Mesh(mData); };
         inline void SetPosition(std::vector<glm::vec3> data) {mData->position = data;}
-        inline void SetUvs(std::vector<glm::vec2> data) {mData->uvs = data;}
+        inline void SetUvs(std::vector<glm::vec2> data) {mData->uv = data;}
         inline void SetNormals(std::vector<glm::vec3> data) {mData->normals = data;}
         inline void SetIndices(std::vector<uint32_t> data) {mData->indices = data;}
         inline std::vector<glm::vec3>& GetPosition() {return mData->position;}
-        inline std::vector<glm::vec2>& GetUvs() {return mData->uvs;}
+        inline std::vector<glm::vec2>& GetUvs() {return mData->uv;}
         inline std::vector<glm::vec3>& GetNormals() {return mData->normals;}
         inline std::vector<uint32_t>& GetIndices() {return mData->indices;}
         inline Data* GetData() {return mData.get();};
@@ -54,7 +92,7 @@ namespace RoxEngine {
         Mesh(Ref<Data>& data){
             mData = CreateRef<Data>();
             mData->position = data->position;
-            mData->uvs = data->uvs;
+            mData->uv = data->uv;
             mData->normals = data->normals;
         }
 
