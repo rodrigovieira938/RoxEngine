effcd6ddd73984eaa00dcdffc12a21e4ab552bbd
Rodrigo Vieira <rodizzvieira@gmail.com>
2026-04-11 18:25:16 +0100

    fix: transform GetMatrix()

--- RoxEngine/src/RoxEngine/renderer/Transform.hpp
+++ RoxEngine/src/RoxEngine/renderer/Transform.hpp
@@ -19,7 +19,7 @@ struct Transform
     glm::mat4 GetMatrix() const
     {
         return glm::translate(glm::mat4(1.0f), translation)
-            * glm::toMat4(glm::quat(rotation))
+            * glm::toMat4(glm::quat(glm::radians(rotation)))
             * glm::scale(glm::mat4(1.0f), scale);
     }
     bool operator ==(const Transform& other) const {
